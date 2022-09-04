62f573848e867b74708680c511b54069828968b4
Farooq <farooq1.khan@ryerson.ca>
2022-09-04 13:51:51 -0400

    intergated tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx,md}",
+      "./components/**/*.{js,ts,tsx,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
