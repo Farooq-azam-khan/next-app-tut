241c0aa38aa1a69833abaafe978d41662fa9bcaf
Farooq <farooq1.khan@ryerson.ca>
2022-09-04 16:21:08 -0400

    build succeeded

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -4,7 +4,7 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 const CodeBlock = ({ children }: any) => {
 
     return (<div className='flex flex-col items-start mt-5'>
-        <CopyToClipboard text={children} className=" px-4 py-2 textinput mb-2">
+        <CopyToClipboard text={children} >
             <button>Copy Code</button>
         </CopyToClipboard>
 
